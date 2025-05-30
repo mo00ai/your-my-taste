e4e1771e32a7eeaf1618d1b52124aa19bd91b6d6
YejinYoon <68535398+YejinY00n@users.noreply.github.com>
2025-05-30 20:20:53 +0900

    [Feature] User, Party, Chat, RandomMatchingCondition 엔티티 #7 (#13)
    * Feat: 유저 엔티티 추가
    
    * Feat: 파티, 파티초대, 유저/파티 매칭 조건 추가
    
    * Feat: Chat 엔티티 추가
    
    * Feat: 생성일 엔티티 상속 적용
    
    * Feat: 유저 생성자 추가
    
    * Feat: 유저 이미지 연관관계 추가

---
+++ src/main/java/com/example/taste/domain/chat/entity/Chat.java
@@ -0,0 +1,37 @@
+package com.example.taste.domain.chat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+
+import com.example.taste.common.entity.BaseCreatedAtEntity;
+import com.example.taste.domain.party.entity.Party;
+import com.example.taste.domain.user.entity.User;
+
+@Entity
+@Getter
+@Table(name = "chat")
+public class Chat extends BaseCreatedAtEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "party_id", nullable = false)
+	private Party party;
+
+	@Column(nullable = false)
+	private String message;
+}
--- src/main/java/com/example/taste/domain/user/entity/User.java
+++ src/main/java/com/example/taste/domain/user/entity/User.java
@@ -1,4 +1,94 @@
 package com.example.taste.domain.user.entity;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.example.taste.common.entity.SoftDeletableEntity;
+import com.example.taste.domain.image.entity.Image;
+import com.example.taste.domain.user.enums.Gender;
+import com.example.taste.domain.user.enums.Level;
+import com.example.taste.domain.user.enums.Role;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends SoftDeletableEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "image_id", nullable = false)
+	private Image image;
+
+	@Column(nullable = false)
+	private String nickname;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	private int age;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Level level = Level.NORMAL;
+
+	@Column(nullable = false)
+	private int postingCount = 0;
+	private int point = 0;
+
+	@Column(nullable = false)
+	private int follower = 0;
+
+	@Column(nullable = false)
+	private int following = 0;
+
+	@Builder
+	public User(String nickname, String email, String password, String address, Gender gender, int age, Role role,
+		Level level, Integer postingCount, Integer point, Integer follower, Integer following) {
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		this.gender = gender;
+		this.age = age;
+		this.role = role;
+		this.level = level == null ? Level.NORMAL : level;
+		this.postingCount = postingCount == null ? postingCount : 0;
+		this.point = point == null ? point : 0;
+		this.follower = follower == null ? point : 0;
+		this.following = following == null ? point : 0;
+	}
 }
