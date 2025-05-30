4a1093aa9e6d30675d0132dc4824eddf47fa9ac9
YejinY00n <yoon17710@gmail.com>
2025-05-30 21:07:09 +0900

    Feat: 팔로우 엔티티 추가

---
+++ src/main/java/com/example/taste/domain/user/entity/Follow.java
@@ -0,0 +1,25 @@
+package com.example.taste.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "follow")
+public class Follow {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+}
