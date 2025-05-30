f1cb82373bf571006cfc9c814d962e315a1c104b
rlgkghkd <rlgkghkd@gmail.com>
2025-05-30 19:37:28 +0900

    refactor: Comment 연관관계 설정

--- src/main/java/com/example/taste/domain/comment/entity/Comment.java
+++ src/main/java/com/example/taste/domain/comment/entity/Comment.java
@@ -3,14 +3,18 @@ package com.example.taste.domain.comment.entity;
 import java.time.LocalDateTime;
 
 import com.example.taste.common.entity.BaseEntity;
+import com.example.taste.domain.board.entity.Board;
+import com.example.taste.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,9 +32,22 @@ public class Comment extends BaseEntity {
 
 	private LocalDateTime deletedAt;
 
-	@Builder
-	public Comment(String content, LocalDateTime deletedAt) {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "commented_user", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "commented_user", nullable = false)
+	private Board board;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_comment")
+	private Comment comment;
+
+	public Comment(String content, User user, Board board, Comment comment) {
 		this.content = content;
-		this.deletedAt = deletedAt;
+		this.user = user;
+		this.board = board;
+		this.comment = comment;
 	}
 }
