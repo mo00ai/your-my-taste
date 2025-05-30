601d8aa2168b914870938861549b17cdaa1d924f
YejinYoon <68535398+YejinY00n@users.noreply.github.com>
2025-05-30 21:44:56 +0900

    Feat#7/init entities (#23)
    * Feat: 유저 엔티티 추가
    
    * Feat: 파티, 파티초대, 유저/파티 매칭 조건 추가
    
    * Feat: Chat 엔티티 추가
    
    * Feat: 생성일 엔티티 상속 적용
    
    * Feat: 유저 생성자 추가
    
    * Feat: 유저 이미지 연관관계 추가
    
    * Feat: 파티 도메인 연관관계 추가
    
    * Feat: 팔로우 엔티티 추가
    
    * Feat: 유저 취향 엔티티 추가
    
    * Feat: 생성자 추가
    
    * Fix: 연관 관계 맵핑 수정
    
    * Fix: 연관 관계 맵핑 수정
    
    * Feat: Favor 생성자 추가
    
    * Feat: Favor 생성자 추가

--- src/main/java/com/example/taste/domain/party/entity/Party.java
+++ src/main/java/com/example/taste/domain/party/entity/Party.java
@@ -14,17 +14,21 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import com.example.taste.common.entity.BaseCreatedAtEntity;
 import com.example.taste.domain.party.enums.PartyStatus;
+import com.example.taste.domain.store.entity.Store;
 import com.example.taste.domain.user.entity.User;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "party")
 public class Party extends BaseCreatedAtEntity {
 	@Id
@@ -36,7 +40,6 @@ public class Party extends BaseCreatedAtEntity {
 	@JoinColumn(name = "user_id", nullable = false)
 	private User hostUser;
 
-	// TODO: 맛집 연관관계
 	@Column(nullable = false)
 	private String title;
 	private String description;
@@ -44,10 +47,29 @@ public class Party extends BaseCreatedAtEntity {
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private PartyStatus partyStatus;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "store_id", nullable = false)
+	private Store store;
+
 	private LocalDateTime meetingTime;
 
 	@Column(nullable = false)
 	private int maxMembers;
 	@Column(nullable = false)
 	private boolean enableRandomMatching = false;
+
+	@Builder
+	public Party(User hostUser, String title, String description, PartyStatus partyStatus, Store store,
+		LocalDateTime meetingTime, int maxMembers, Boolean enableRandomMatching) {
+		this.hostUser = hostUser;
+		this.title = title;
+		this.description = description;
+		this.partyStatus = partyStatus;
+		this.store = store;
+		this.meetingTime = meetingTime;
+		this.maxMembers = maxMembers;
+		this.enableRandomMatching = enableRandomMatching != null ? enableRandomMatching : false;
+	}
 }
