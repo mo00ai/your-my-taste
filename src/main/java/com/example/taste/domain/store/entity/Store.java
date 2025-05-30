70e66a8933980220cdd386d0ea55bce79f7e7707
kmchaejin <109498876+kmchaejin@users.noreply.github.com>
2025-05-30 20:42:47 +0900

    [Feature] store 도메인 관련 entity에 연관관계 적용 및 리팩토링 (#14)
    * ✨feat : store 도메인 엔티티 생성
    
    * ✨ feat : 기본 생성자 및 빌더 추가
    
    * ✨ feat : repository 틀 생성 및 빌더 적용
    
    * ✨ feat : 생성자 어노테이션 추가
    
    * ✨ feat : Enumerated 어노테이션 적용
    
    * ✨ feat : Getter 어노테이션 적용
    
    * ♻️  refactor : User 연관관계 추가 및 어노테이션 적용

--- src/main/java/com/example/taste/domain/store/entity/Store.java
+++ src/main/java/com/example/taste/domain/store/entity/Store.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "store")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class Store extends BaseEntity {
--- src/main/java/com/example/taste/domain/store/entity/StoreBucket.java
+++ src/main/java/com/example/taste/domain/store/entity/StoreBucket.java
@@ -1,12 +1,21 @@
 package com.example.taste.domain.store.entity;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.example.taste.domain.user.entity.User;
+
 import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,13 +23,18 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "store_bucket")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class StoreBucket {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
 	@Column(nullable = false)
 	private String name;
@@ -29,7 +43,8 @@ public class StoreBucket {
 	private boolean isOpened;
 
 	@Builder
-	public StoreBucket(String name, Boolean isOpened) {
+	public StoreBucket(User user, String name, Boolean isOpened) {
+		this.user = user;
 		this.name = StringUtils.isBlank(name) ? "기본 리스트" : name;
 		this.isOpened = isOpened == null || isOpened;
 	}
