a484ca2fd65f25d6912acf590f87fa50a5fbe680
AhyunCho <jah112482@gmail.com>
2025-05-30 12:47:01 +0900

    chore : setting 오류 수정

--- src/main/java/com/example/taste/common/entity/BaseEntity.java
+++ src/main/java/com/example/taste/common/entity/BaseEntity.java
@@ -18,8 +18,8 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDateTime modifyAt;
+    private LocalDateTime updatedAt;
 }
