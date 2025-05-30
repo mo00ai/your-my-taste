4384f57afcc581901767b764071c5aaf12a0ca94
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-05-30 19:06:42 +0900

    [Feature] Image, Pk 관련 entity 생성 (#11)
    * feat : image entity 구현
    
    * feat : BaseCreatedEntity 생성
    
    * feat : Pk 관련 Entity,enum 파일  생성
    
    * style : checkstyle 반영
    
    * refactor : builder 수정
    
    * refactor : enumerated 추가
    
    * refactor : rank 예약어 오류 수정

---
+++ src/main/java/com/example/taste/domain/pk/entity/PkCriteria.java
@@ -0,0 +1,45 @@
+package com.example.taste.domain.pk.entity;
+
+import com.example.taste.domain.pk.enums.PkType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "pk_criteria")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PkCriteria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PkType pkType;
+
+	@Column(nullable = false)
+	private int count;
+
+	@Column(nullable = false)
+	private boolean isActive;
+
+	@Builder
+	public PkCriteria(PkType pkType, Integer count, Boolean isActive) {
+		this.pkType = pkType;
+		this.count = count != null ? count : 0;
+		this.isActive = isActive != null ? isActive : true;
+	}
+
+}
