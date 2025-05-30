e5906e04c5543ab114eae47bdd189e245f299f9f
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-05-30 19:06:42 +0900

    [Feature] Image, Pk 관련 entity 생성 (#11)
    * feat : image entity 구현
    
    * feat : BaseCreatedEntity 생성
    
    * feat : Pk 관련 Entity,enum 파일  생성
    
    * style : checkstyle 반영
    
    * refactor : builder 수정
    
    * refactor : enumerated 추가
    
    * refactor : rank 예약어 오류 수정

---
+++ src/main/java/com/example/taste/domain/pk/entity/PkLog.java
@@ -0,0 +1,44 @@
+package com.example.taste.domain.pk.entity;
+
+import com.example.taste.common.entity.BaseCreatedAtEntity;
+import com.example.taste.domain.pk.enums.PkType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "pk_log")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PkLog extends BaseCreatedAtEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PkType pkType;
+
+	@Column(nullable = false)
+	private int point = 0;
+
+	//user와 연관관계
+
+	@Builder
+	public PkLog(PkType pkType, Integer point) {
+		this.pkType = pkType;
+		this.point = point != null ? point : 0;
+	}
+
+}
