ccdf0cb7d5fd4b3c0510d36c7d1f4e22722128a6
AhyunCho <jah112482@gmail.com>
2025-06-02 20:12:17 +0900

    fix : 주석 삭제, 불필요한 코드 제거

--- src/main/java/com/example/taste/domain/image/service/ImageService.java
+++ src/main/java/com/example/taste/domain/image/service/ImageService.java
@@ -14,7 +14,6 @@ import com.example.taste.common.exception.CustomException;
 import com.example.taste.domain.image.dto.ImageResponseDto;
 import com.example.taste.domain.image.entity.Image;
 import com.example.taste.domain.image.enums.ImageType;
-import com.example.taste.domain.image.repository.BoardImageRepository;
 import com.example.taste.domain.image.repository.ImageRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -25,74 +24,6 @@ public class ImageService {
 
 	private final S3Service s3Service;
 	private final ImageRepository imageRepository;
-	private final BoardImageRepository boardImageRepository;
-
-	// //aws에 업로드
-	// public List<Map<String, Object>> uploadFile(List<MultipartFile> fileList) throws IOException {
-	// 	List<Map<String, Object>> returnList = new ArrayList<>(); // 업로드한 사진 정보 담아서 리턴
-	//
-	// 	for (MultipartFile file : fileList) {
-	// 		Map<String, Object> fileInfo = new HashMap<>();
-	//
-	// 		String originalFilename = file.getOriginalFilename();
-	// 		String uploadFilename = UUID.randomUUID() + "_" + file.getOriginalFilename();
-	// 		Long fileSize = file.getSize();
-	// 		String fileExtension = originalFilename.substring(originalFilename.lastIndexOf('.') + 1).toLowerCase();
-	//
-	// 		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-	// 			.bucket(bucketName)  // 업로드할 대상 버킷 이름
-	// 			.key(uploadFilename)  // 버킷 내 저장할 경로 (위에서 만든 고유 파일명)
-	// 			.contentType(file.getContentType()) // 타입 image/png
-	// 			.build();
-	// 		s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
-	//
-	// 		fileInfo.put("url", "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + uploadFilename);
-	// 		fileInfo.put("originalFilename", originalFilename);
-	// 		fileInfo.put("uploadFilename", uploadFilename);
-	// 		fileInfo.put("fileSize", fileSize);
-	// 		fileInfo.put("fileExtension", fileExtension);
-	// 		fileInfo.put("contentType", file.getContentType());
-	//
-	// 		returnList.add(fileInfo);
-	// 	}
-	// 	return returnList;
-	// }
-
-	// //aws에 사진 업로드 후 db에 저장
-	// public void saveImage(List<MultipartFile> files, ImageType type) {
-	//
-	// 	List<String> uploadedKey = new ArrayList<>(); // 업로드 성공한 키 저장
-	// 	try {// db에 저장 실패 or aws 에 업로드 실패 시 에러
-	// 		uploadFile(files).forEach(data -> {
-	// 			String url = (String)data.get("url");
-	// 			String originalName = (String)data.get("originalFilename");
-	// 			String uploadName = (String)data.get("uploadFilename");
-	// 			String extension = (String)data.get("fileExtension");
-	// 			Long size = (Long)data.get("fileSize");
-	//
-	// 			Image image = Image.builder()
-	// 				.type(type)
-	// 				.url(url)
-	// 				.originFileName(originalName)
-	// 				.uploadFileName(uploadName)
-	// 				.fileExtension(extension)
-	// 				.build();
-	//
-	// 			imageRepository.save(image);
-	//
-	// 			uploadedKey.add(uploadName);
-	// 		});
-	// 	} catch (Exception e) {
-	// 		for (String key : uploadedKey) {
-	// 			DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder() // aws 에 이미 올라간 이미지 삭제
-	// 				.bucket(bucketName)  // 연결 된 대상 버킷 이름
-	// 				.key(key)  // 버킷 내 삭제할 객체 키
-	// 				.build();
-	// 			s3Client.deleteObject(deleteObjectRequest);
-	// 		}
-	// 		throw new CustomException(ErrorCode.FILE_UPLOAD_FAILED); // 리뷰 저장 롤백위해 오류 날림
-	// 	}
-	// }
 
 	@Transactional
 	public ImageResponseDto saveImage(MultipartFile file, ImageType type) throws IOException {
@@ -138,7 +69,9 @@ public class ImageService {
 		Image image = imageRepository.findById(imageId)
 			.orElseThrow(() -> new CustomException(IMAGE_NOT_FOUND));
 
-		return new ImageResponseDto(image);
+		return ImageResponseDto.builder()
+			.image(image)
+			.build();
 	}
 
 	@Transactional
@@ -147,7 +80,6 @@ public class ImageService {
 			.orElseThrow(() -> new CustomException(IMAGE_NOT_FOUND));
 
 		deleteImage(image);// 기존 사진 삭제
-		s3Service.upload(file);
 		saveImage(file, type);
 	}
 
