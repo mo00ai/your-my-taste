cb7062461f40eb4aa5d80bc86bb2a497e6869cbf
Chaemin07 <gkc05075@gmail.com>
2025-06-02 15:04:55 +0900

    feat: 네이버 지도 API 설정 클래스 및 WebClient 설정 추가
    - NaverConfig 클래스 생성해 clientId, secret, geocoding URL 등 프로퍼티 바인딩 처리
    - WebClientConfig 클래스 생성해 WebClient Bean 등록

--- src/main/java/com/example/taste/TasteApplication.java
+++ src/main/java/com/example/taste/TasteApplication.java
@@ -2,8 +2,12 @@ package com.example.taste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.example.taste.config.NaverConfig;
+
+@EnableConfigurationProperties(NaverConfig.class)
 @EnableJpaAuditing
 @SpringBootApplication
 public class TasteApplication {
