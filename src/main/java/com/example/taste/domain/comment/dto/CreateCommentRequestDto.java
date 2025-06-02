28dbcd6d58bb67f638ad0f82b8a9cb4be22008a9
rlgkghkd <rlgkghkd@gmail.com>
2025-06-02 11:26:07 +0900

    [Feature] Comment CRUD #10 (#25)
    * refactor: Comment 연관관계 설정
    
    * feat: create comment controller, service, repository, dto for create request
    
    * [Feature] Board, Hashtag, Like 관련 entity 생성 #6 (#12)
    
    * feat: 게시글(Board) 엔티티 생성(연관관계 제외)
    
    - BoardType, BoardStatus enum 매핑 적용
    
    * feat: 게시글-해시태그 연관관계 설정 및 이벤트 관련 엔티티 생성
    
    - Board ↔ BoardHashtag 양방향 연관관계 설정 (게시글 기준 해시태그 조회)
    - BoardHashtag → Hashtag 단방향 다대일 연관관계 설정
    - 연관관계 일관성 유지를 위한 편의 메서드 구현
    - Like, Event, BoardEvent 엔티티 생성
    - 게시글과 이벤트 간 연관관계 설정
    - 모든 엔티티 생성자에 빌더 패턴 적용 (팀 규칙 반영)
    
    * rename: Like 엔티티 테이블명을 'likes'로 변경
    
    - 예약어 'like'로 인한 충돌 방지를 위해 테이블명을 'likes'로 수정
    
    * refactor: Event 엔티티 필드에 @Column(nullable = false) 설정 추가
    
    * refactor: BoardEvent 생성자에서 잘못된 void 반환 타입 제거
    
    * [Feature] User, Party, Chat, RandomMatchingCondition 엔티티 #7 (#13)
    
    * Feat: 유저 엔티티 추가
    
    * Feat: 파티, 파티초대, 유저/파티 매칭 조건 추가
    
    * Feat: Chat 엔티티 추가
    
    * Feat: 생성일 엔티티 상속 적용
    
    * Feat: 유저 생성자 추가
    
    * Feat: 유저 이미지 연관관계 추가
    
    * feat: add comment creation
    
    * feat:add update comment
    
    * feat: add update, soft delete api
    
    * feat: add getAllComment api
    
    * feat: add api to get single comment
    
    * feix: add parent, root, child relationships to Comment
    
    * refactor: separate root and nested comments for pagination
    
    * refactor: remove unnecessary ResponseEntity
    
    * fix: corret delete mapping
    
    ---------
    
    Co-authored-by: modesty <108976049+Chaemin07@users.noreply.github.com>
    Co-authored-by: YejinYoon <68535398+YejinY00n@users.noreply.github.com>

---
+++ src/main/java/com/example/taste/domain/comment/dto/CreateCommentRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.taste.domain.comment.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCommentRequestDto {
+
+	@NotNull
+	//@Pattern(regexp = "^(?!.*(<script|</script|<img|<a\\s|select\\s|union\\s|insert\\s|update\\s|delete\\s|drop\\s|--|\\bor\\b|\\band\\b)).*$", message = "허용되지 않는 문자가 포함되어있습니다.")
+	//굳이?
+	private String content;
+	private Long parent;
+}
