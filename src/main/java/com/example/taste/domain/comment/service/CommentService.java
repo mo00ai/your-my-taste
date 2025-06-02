a4ea5f1a0b761fa26be2c03e62aa5e147bdbdf30
rlgkghkd <rlgkghkd@gmail.com>
2025-06-02 11:26:07 +0900

    [Feature] Comment CRUD #10 (#25)
    * refactor: Comment 연관관계 설정
    
    * feat: create comment controller, service, repository, dto for create request
    
    * [Feature] Board, Hashtag, Like 관련 entity 생성 #6 (#12)
    
    * feat: 게시글(Board) 엔티티 생성(연관관계 제외)
    
    - BoardType, BoardStatus enum 매핑 적용
    
    * feat: 게시글-해시태그 연관관계 설정 및 이벤트 관련 엔티티 생성
    
    - Board ↔ BoardHashtag 양방향 연관관계 설정 (게시글 기준 해시태그 조회)
    - BoardHashtag → Hashtag 단방향 다대일 연관관계 설정
    - 연관관계 일관성 유지를 위한 편의 메서드 구현
    - Like, Event, BoardEvent 엔티티 생성
    - 게시글과 이벤트 간 연관관계 설정
    - 모든 엔티티 생성자에 빌더 패턴 적용 (팀 규칙 반영)
    
    * rename: Like 엔티티 테이블명을 'likes'로 변경
    
    - 예약어 'like'로 인한 충돌 방지를 위해 테이블명을 'likes'로 수정
    
    * refactor: Event 엔티티 필드에 @Column(nullable = false) 설정 추가
    
    * refactor: BoardEvent 생성자에서 잘못된 void 반환 타입 제거
    
    * [Feature] User, Party, Chat, RandomMatchingCondition 엔티티 #7 (#13)
    
    * Feat: 유저 엔티티 추가
    
    * Feat: 파티, 파티초대, 유저/파티 매칭 조건 추가
    
    * Feat: Chat 엔티티 추가
    
    * Feat: 생성일 엔티티 상속 적용
    
    * Feat: 유저 생성자 추가
    
    * Feat: 유저 이미지 연관관계 추가
    
    * feat: add comment creation
    
    * feat:add update comment
    
    * feat: add update, soft delete api
    
    * feat: add getAllComment api
    
    * feat: add api to get single comment
    
    * feix: add parent, root, child relationships to Comment
    
    * refactor: separate root and nested comments for pagination
    
    * refactor: remove unnecessary ResponseEntity
    
    * fix: corret delete mapping
    
    ---------
    
    Co-authored-by: modesty <108976049+Chaemin07@users.noreply.github.com>
    Co-authored-by: YejinYoon <68535398+YejinY00n@users.noreply.github.com>

---
+++ src/main/java/com/example/taste/domain/comment/service/CommentService.java
@@ -0,0 +1,119 @@
+package com.example.taste.domain.comment.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.taste.domain.board.entity.Board;
+import com.example.taste.domain.comment.dto.CreateCommentRequestDto;
+import com.example.taste.domain.comment.dto.CreateCommentResponseDto;
+import com.example.taste.domain.comment.dto.DeleteCommentResponseDto;
+import com.example.taste.domain.comment.dto.GetCommentDto;
+import com.example.taste.domain.comment.dto.UpdateCommentRequestDto;
+import com.example.taste.domain.comment.dto.UpdateCommentResponseDto;
+import com.example.taste.domain.comment.entity.Comment;
+import com.example.taste.domain.comment.repository.CommentRepository;
+import com.example.taste.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+	private final CommentRepository commentRepository;
+	//private final BoardRepository boardRepository;
+
+	public CreateCommentResponseDto createComment(CreateCommentRequestDto requestDto, Long boardsId) {
+		Board board = Board.builder().build();
+		User user = new User(); //세션에서 가져올 것
+		Comment parent = requestDto.getParent() == null ? null :
+			commentRepository.findById(requestDto.getParent()).orElseThrow();
+		Comment root = parent == null ? null : parent.getRoot() == null ? parent : parent.getRoot();
+
+		Comment comment = Comment.builder()
+			.content(requestDto.getContent())
+			.parent(parent)
+			.root(root)
+			.board(board)
+			.user(user)
+			.build();
+
+		Comment saved = commentRepository.save(comment);
+
+		return new CreateCommentResponseDto(saved);
+	}
+
+	@Transactional
+	public UpdateCommentResponseDto updateComment(UpdateCommentRequestDto requestDto, Long commentId) {
+		Comment comment = commentRepository.findById(commentId).orElseThrow();
+		comment.updateContent(requestDto.getContent());
+		return new UpdateCommentResponseDto(comment);
+	}
+
+	@Transactional
+	public DeleteCommentResponseDto deleteComment(Long commentId) {
+		Comment comment = commentRepository.findById(commentId).orElseThrow();
+		comment.deleteContent(LocalDateTime.now());
+		return new DeleteCommentResponseDto(comment);
+	}
+
+	public Page<GetCommentDto> getAllCommentOfBoard(Long boardsId, int index) {
+		// 연관관계를 통한 N+1 문제가 발생하지 않도록, root comment와 거기에 달린 comment들을 연관관계에 의존하지 않고 가져온다.
+		Board board = Board.builder().build();
+		Pageable pageable = PageRequest.of(index, 10);
+		// board에 달린 댓글 중에서 root인 댓글들만 가져온다.(root == null 인 케이스)
+		Page<Comment> comments = commentRepository.findAllRootByBoard(board, pageable);
+		//모든 댓글 중에서 위의 comments들을 root로 가진 댓글들만 가져온다(n+1 회피)
+		List<Comment> replies = commentRepository.findAllReplies(comments);
+		// 이런 식이면 댓글을 가져오고 거기의 child를 찾아 가져오고, 그 댓글의 child를 다시 가져오고...
+		// 하는 방식의 n+1을 피할 수 있다.
+
+		// root list는 나중에 page로 전환하기 위한 용도
+		// root map은 대댓글을 달기 위한 root를 찾는 용도(map이 list보다 조회하기 편하다)
+		List<GetCommentDto> rootsList = new ArrayList<>();
+		Map<Long, GetCommentDto> rootsMap = new HashMap<>();
+		// root 댓글을 전부 dto 화 시킨다.
+		// dto들을 rootList와 rootMap에 저장한다.
+		for (Comment comment : comments.getContent()) {
+			GetCommentDto dto = new GetCommentDto(comment);
+			rootsList.add(dto);
+			rootsMap.put(dto.getId(), dto);
+		}
+		// 대댓글을 검색하기 쉽게 넣어줄 tempMap.
+		// 대댓글을 검색해야 하는 이유는 root에 달린 1차 대댓글이 아니라 대댓글에 달린 n차 대댓글인 경우 그 대댓글의 child로 달려야 하기 때문
+		Map<Long, GetCommentDto> temp = new HashMap<Long, GetCommentDto>();
+
+		// 대댓글을 전부 순회한다.
+		for (Comment comment : replies) {
+			// 일단 대댓글을 dto로 만들고
+			GetCommentDto dto = new GetCommentDto(comment);
+			// tempMap 에 넣는다.
+			temp.put(dto.getId(), dto);
+
+			// 현재 조회하는 대댓글이 부모와 root 필드가 일치하는 경우
+			// 이 대댓글은 root 댓글에 달린 1차 대댓글 이므로, root의 child로 등록된다.
+			if (comment.getParent().getId().equals(comment.getRoot().getId())) {
+				rootsMap.get(comment.getParent().getId()).getChildren().add(dto);
+			} else {
+				// 아닌 경우 n차 대댓글이므로, 대댓글 map에서 부모 대댓글을 찾아 child로 등록한다.
+				temp.get(comment.getParent().getId()).getChildren().add(dto);
+			}
+		}
+
+		// page를 만들어서 반환한다. 이 때 page의 메타데이터를 기존의 comments에서 가져온다.
+		return new PageImpl<>(rootsList, comments.getPageable(), comments.getTotalElements());
+	}
+
+	public GetCommentDto getComment(Long commentId) {
+		return new GetCommentDto(commentRepository.findById(commentId).orElseThrow());
+	}
+}
