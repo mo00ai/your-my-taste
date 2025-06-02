dcb3848e0ad5a02e13a38eb41c09c7b4564eb485
Chaemin07 <gkc05075@gmail.com>
2025-06-02 15:05:46 +0900

    feat: 네이버 지도 API 요청 및 응답 DTO 클래스 생성
    - Geocoding 및 ReverseGeocoding API 호출을 위한 요청/응답 DTO 정의

---
+++ src/main/java/com/example/taste/domain/map/dto/geo/GeoAddress.java
@@ -0,0 +1,38 @@
+package com.example.taste.domain.map.dto.geo;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeoAddress {
+	/**
+	 *	roadAddress: 도로명 주소
+	 */
+	private String roadAddress;
+	/**
+	 * jibunAddress: 지번 주소
+	 */
+	private String jibunAddress;
+	/**
+	 * 	AddressElements: 주소 구성 요소 정보
+	 */
+	private List<GeoAddressElements> addressElements;
+	/**
+	 * 	longitude: X 좌표(경도)
+	 */
+	@JsonProperty("x")
+	private String longitude;
+	/**
+	 * latitue: Y 좌표(위도)
+	 */
+	@JsonProperty("y")
+	private String latitude;
+
+}
