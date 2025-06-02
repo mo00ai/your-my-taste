70bb36b7fa02bf9865456ad6147ff18dd1e4c098
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-06-02 21:02:48 +0900

    [Feature] Image Service 구현 (#29)
    * feat : image entity 구현
    
    * feat : BaseCreatedEntity 생성
    
    * feat : Pk 관련 Entity,enum 파일  생성
    
    * style : checkstyle 반영
    
    * refactor : builder 수정
    
    * refactor : enumerated 추가
    
    * refactor : rank 예약어 오류 수정
    
    * feat : image, pk entity들 연관관계 추가
    
    * feat : image S3 Service 분리
    
    * feat : image 테스트 전, 기능 구현 완료, 수정 필요
    
    * fix : 테스트 하면서 수정함
    
    * fix : PR template 위치 수정
    
    * fix : 주석 삭제, 불필요한 코드 제거
    
    * fix : ImageService update 수정
    
    * fix : ImageResponseDto builder 삭제 후 생성자 사용

---
+++ src/main/java/com/example/taste/domain/image/repository/BoardImageRepository.java
@@ -0,0 +1,20 @@
+package com.example.taste.domain.image.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.taste.domain.image.dto.ImageResponseDto;
+import com.example.taste.domain.image.entity.BoardImage;
+
+public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
+
+	@Query("select new com.example.taste.domain.image.dto.ImageResponseDto(bi.image.id, bi.image.url,bi.image.uploadFileName) from BoardImage bi where bi.board.id = :boardId")
+	List<ImageResponseDto> findAllById(@Param("boardId") Long boardId);
+
+	@Query("select bi from BoardImage bi where bi.board.id = :boardId")
+	List<BoardImage> findImagesById(@Param("boardId") Long boardId);
+
+}
