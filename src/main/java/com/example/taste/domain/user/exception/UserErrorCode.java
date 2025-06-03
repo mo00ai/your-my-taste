4ded6d856b929fd528e94e6dc9ec932357086a79
YejinY00n <yoon17710@gmail.com>
2025-06-04 02:22:18 +0900

    Feat: 스프링 시큐리티 설정

--- src/main/java/com/example/taste/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/taste/domain/user/exception/UserErrorCode.java
@@ -11,6 +11,8 @@ import com.example.taste.common.exception.BaseCode;
 @RequiredArgsConstructor
 public enum UserErrorCode implements BaseCode {
 	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "U001", "비밀번호가 일치하지 않습니다."),
+	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "U002", "해당 유저를 찾을 수 없습니다."),
+	DEACTIVATED_USER(HttpStatus.BAD_REQUEST, "U002", "해당 유저를 찾을 수 없습니다."),
 	;
 
 	private final HttpStatus httpStatus;
--- src/main/java/com/example/taste/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/taste/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.taste.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.taste.domain.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }
