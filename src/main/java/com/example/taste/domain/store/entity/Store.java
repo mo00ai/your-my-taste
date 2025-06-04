d2f909d4256960a6943aa9e653c8b21515cd31e1
Chaemin07 <gkc05075@gmail.com>
2025-06-04 14:27:06 +0900

    style: 테스트를 위해 @NoArgsConstructor access 속성 제거
    - 추후 접근제한자 복원 필요 여부 검토 예정

--- src/main/java/com/example/taste/domain/store/entity/Store.java
+++ src/main/java/com/example/taste/domain/store/entity/Store.java
@@ -18,7 +18,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,7 +25,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "store")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class Store extends BaseEntity {
