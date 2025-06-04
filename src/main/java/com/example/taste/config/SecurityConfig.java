fd48bbaa10c4fa379d387247e9cc33557d1ce2bd
rlgkghkd <rlgkghkd@gmail.com>
2025-06-04 19:23:02 +0900

    [Feature] Review CRUD, Comment Review chore #38
    * feat: Review create dto
    
    * feat, fix: add Review creation, fix comment request annotations
    
    * chore: fix typo
    
    * feat: add update, get Review api
    
    * feat: add review deletion api, receipt validation api
    
    * refactor: apply custom error
    
    * fix: change controller request param to MultipartFile
    
    * refactor: replace temporal objects
    
    * refactor: replace temporal entities, use custom error of ReviewService
    
    * refactor: replace temporal entities, use custom error of CommentService
    
    * refactor: add isPresented field to review creation, update dto
    
    * chore: security 임시 무력화
    
    * fix: typo, unignore annotation, add pattern annotations
    
    * fix: match data type int-> inteager
    
    * fix: paging at db level
    
    * fix: Review entity, match type, remove redundant validation setting
    
    * fix: replace requestbody to requestpart, add null check for image files
    
    * fix: prevent image npe, create and use ocr failed custom error
    
    * fix: fecting store id instead of user id for redis search key
    
    * fix: contents empty check, correct key typo, use custom exception when fetching store
    
    * fix: fix key typo, remove image npe check from service
    
    * fix: add image npe check at service

--- src/main/java/com/example/taste/config/SecurityConfig.java
+++ src/main/java/com/example/taste/config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.example.taste.config;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
-// @EnableWebSecurity
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	//임시 무력화
+	/*
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+	*/
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
---
+++ src/main/java/com/example/taste/domain/review/repository/ReviewRepositoryImpl.java
@@ -0,0 +1,63 @@
+package com.example.taste.domain.review.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import com.example.taste.domain.image.entity.QImage;
+import com.example.taste.domain.review.entity.QReview;
+import com.example.taste.domain.review.entity.Review;
+import com.example.taste.domain.store.entity.QStore;
+import com.example.taste.domain.store.entity.Store;
+import com.example.taste.domain.user.entity.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
+	@Override
+	public Optional<Review> getReviewWithUserAndStore(Long reviewId) {
+		QReview qReview = QReview.review;
+		QUser qUser = QUser.user;
+		QStore qStore = QStore.store;
+		return Optional.ofNullable(queryFactory.selectFrom(qReview)
+			.leftJoin(qReview.user, qUser).fetchJoin()
+			.leftJoin(qReview.store, qStore).fetchJoin()
+			.where(
+				qReview.id.eq(reviewId)
+			).fetchFirst());
+	}
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public Page<Review> getAllReview(Store store, Pageable pageable, int score) {
+		QImage qImage = QImage.image;
+		QReview qReview = QReview.review;
+
+		long total = queryFactory.selectFrom(qReview)
+			.where(
+				qReview.store.eq(store),
+				qReview.isPresented.isTrue(),
+				score == 0 ? null : qReview.score.eq(score)
+			)
+			.fetchCount();
+
+		List<Review> reviews = queryFactory.selectFrom(qReview)
+			.leftJoin(qReview.image, qImage).fetchJoin()
+			.where(
+				qReview.store.eq(store),
+				qReview.isPresented.isTrue(),
+				score == 0 ? null : qReview.score.eq(score)
+			)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		return new PageImpl<Review>(reviews, pageable, total);
+	}
+}
