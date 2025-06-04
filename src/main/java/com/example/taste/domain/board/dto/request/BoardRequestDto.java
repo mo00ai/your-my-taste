7ec03ce605217a43012909c2bcec994ee10064f8
Chaemin07 <gkc05075@gmail.com>
2025-06-04 16:37:13 +0900

    feat: 요청 DTO에 유효성 검사 어노테이션 추가
    - @Valid를 통한 컨트롤러 단 유효성 검사 적용

--- src/main/java/com/example/taste/domain/board/dto/request/BoardRequestDto.java
+++ src/main/java/com/example/taste/domain/board/dto/request/BoardRequestDto.java
@@ -3,6 +3,9 @@ package com.example.taste.domain.board.dto.request;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @JsonTypeInfo(
@@ -17,10 +20,19 @@ import lombok.Getter;
 })
 @Getter
 public abstract class BoardRequestDto {
+	@NotBlank(message = "제목은 필수입니다.")
+	@Size(max = 100, message = "제목은 100자 이내여야 합니다.")
 	private String title;
+
+	@NotBlank(message = "내용은 필수입니다.")
+	@Size(max = 1000, message = "내용은 1000자 이내여야 합니다.")
 	private String contents;
+
+	@Pattern(regexp = "^(NH)$", message = "게시글 타입은 'N' 또는 'H'만 허용됩니다.")
 	private String type;
+
 	private String status;
+
 	private Long storeId;
 
 }
