d1c29075f622c1919a43fa4ea0405ef491146d40
YejinY00n <yoon17710@gmail.com>
2025-06-04 13:32:41 +0900

    Feat: 로그아웃 구현

--- src/main/java/com/example/taste/domain/auth/dto/SignupRequestDto.java
+++ src/main/java/com/example/taste/domain/auth/dto/SignupRequestDto.java
@@ -12,7 +12,7 @@ import org.hibernate.validator.constraints.Range;
 import com.example.taste.domain.user.dto.request.UserFavorUpdateListRequestDto;
 
 @Getter
-public class SignUpRequestDto {
+public class SignupRequestDto {
 	@NotBlank(message = "이메일은 필수값입니다.")
 	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "유효한 이메일 형식이 아닙니다.")
 	private String email;
