0834d3cd4e8304b0629b627921fcd7a270ffb215
Chaemin07 <gkc05075@gmail.com>
2025-06-04 19:59:55 +0900

    feat: 게시글 타입 미존재 시 예외 처리 로직 추가
    - 유효하지 않은 타입 입력 시 BOARD_TYPE_NOT_FOUND 예외 발생

--- src/main/java/com/example/taste/domain/board/exception/BoardErrorCode.java
+++ src/main/java/com/example/taste/domain/board/exception/BoardErrorCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 public enum BoardErrorCode implements BaseCode {
 
 	BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "B001", "게시글을 찾을 수 없습니다."),
-	
+
+	BOARD_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "BT001", "존재하지 않는 게시글 타입입니다."),
 	;
 
 	private final HttpStatus httpStatus;
