2c053064a48f16b2946049b5f6efbf968d86f5b7
YejinY00n <yoon17710@gmail.com>
2025-06-04 18:16:51 +0900

    Refactor: UserErrorCode 수정

--- src/main/java/com/example/taste/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/taste/domain/user/exception/UserErrorCode.java
@@ -12,10 +12,10 @@ import com.example.taste.common.exception.BaseCode;
 public enum UserErrorCode implements BaseCode {
 	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "U001", "비밀번호가 일치하지 않습니다."),
 	NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "U002", "해당 유저를 찾을 수 없습니다."),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U002", "유저 정보가 존재하지 않습니다."),
-	DEACTIVATED_USER(HttpStatus.BAD_REQUEST, "U003", "탈퇴한 유저입니다"),
-	CONFLICT_EMAIL(HttpStatus.BAD_REQUEST, "U004", "중복 이메일입니다."),
-	
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U003", "유저 정보가 존재하지 않습니다."),
+	DEACTIVATED_USER(HttpStatus.BAD_REQUEST, "U004", "탈퇴한 유저입니다"),
+	CONFLICT_EMAIL(HttpStatus.BAD_REQUEST, "U005", "중복 이메일입니다."),
+
 	FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "F001", "팔로우 정보가 존재하지 않습니다."),
 	;
 
