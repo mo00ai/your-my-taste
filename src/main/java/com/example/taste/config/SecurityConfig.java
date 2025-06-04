79f3e47b9e83d65da54ac8bbeb408e3a205b4276
rlgkghkd <rlgkghkd@gmail.com>
2025-06-04 16:50:43 +0900

    chore: security 임시 무력화

--- src/main/java/com/example/taste/config/SecurityConfig.java
+++ src/main/java/com/example/taste/config/SecurityConfig.java
@@ -1,19 +1,31 @@
 package com.example.taste.config;
 
-//import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
 
-//@Configuration
-// @EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//}
+	//임시 무력화
+	/*
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
+	*/
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
