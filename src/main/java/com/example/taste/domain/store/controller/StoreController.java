a514c312c910cf0fb81dbbff1bd88b577de59eae
kmchaejin <109498876+kmchaejin@users.noreply.github.com>
2025-06-04 11:46:16 +0900

    [Feature] Store CRUD (#33)
    * ✨feat : createBucket 구현
    
    * ✨feat : addStore 구현
    
    * ✨feat : getStore 구현
    
    * ✨feat : 기본 crud 구현
    
    * ✨feat : 기본 crud 구현
    
    * ✨feat : 유효성 검증
    
    * 🚚move : 유저 관련 error code 이동
    
    * 🚚 move : User 도메인 errorcode 이동
    
    * 🐛fix : USER_NOT_FOUND status 수정
    
    * ✨feat : valid 추가 및  message 옵션 적용
    
    * ♻️ refactor : 코드 통일 및 명확성을 위해 enum 클래스 수정
    
    * 🐛 fix : NPE 방지
    
    * 🐛fix : 컨트롤러-서비스 연결안된 부분 연결
    
    * 🐛fix : addBucketItem 트랜잭션 적용
    
    * ♻️ refactor : 버킷명 중복 확인 로직 분리
    
    * ♻️ refactor : validated 에러 핸들러 리턴 타입 수정
    
    * 🔥remove : jpaRepo에서 제공하는 메서드 중복 삭제

---
+++ src/main/java/com/example/taste/domain/store/controller/StoreController.java
@@ -0,0 +1,35 @@
+package com.example.taste.domain.store.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.taste.common.response.CommonResponse;
+import com.example.taste.domain.store.dto.response.StoreResponse;
+import com.example.taste.domain.store.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/stores")
+public class StoreController {
+	private final StoreService storeService;
+
+	@GetMapping("/{id}")
+	public CommonResponse<StoreResponse> getStore(@PathVariable Long id) {
+		return CommonResponse.ok(storeService.getStore(id));
+	}
+
+	//@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteStore(@PathVariable Long id) {
+		storeService.deleteStore(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT)
+			.body(null);
+	}
+}
