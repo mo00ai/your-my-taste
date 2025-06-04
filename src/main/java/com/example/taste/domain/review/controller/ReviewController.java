5ed07b062d93dc91f362a9936de2bd031c233648
rlgkghkd <rlgkghkd@gmail.com>
2025-06-04 17:32:12 +0900

    fix: replace requestbody to requestpart, add null check for image files

--- src/main/java/com/example/taste/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/taste/domain/review/controller/ReviewController.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.taste.common.annotation.ImageValid;
+import com.example.taste.common.exception.CustomException;
 import com.example.taste.common.response.CommonResponse;
 import com.example.taste.domain.image.enums.ImageType;
+import com.example.taste.domain.image.exception.ImageErrorCode;
 import com.example.taste.domain.review.dto.CreateReviewRequestDto;
 import com.example.taste.domain.review.dto.CreateReviewResponseDto;
 import com.example.taste.domain.review.dto.GetReviewResponseDto;
@@ -35,20 +37,28 @@ public class ReviewController {
 	private final ReviewService reviewService;
 
 	@ImageValid
-	@PostMapping
-	public CommonResponse<CreateReviewResponseDto> createReview(@RequestBody CreateReviewRequestDto requestDto,
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public CommonResponse<CreateReviewResponseDto> createReview(
+		@RequestPart("requestDto") CreateReviewRequestDto requestDto,
 		@PathVariable Long storeId,
 		@RequestPart(value = "files", required = false) List<MultipartFile> files)
 		throws IOException {
+		if (files == null || files.isEmpty()) {
+			throw new CustomException(ImageErrorCode.IMAGE_NOT_FOUND);
+		}
 		return CommonResponse.created(reviewService.createReview(requestDto, storeId, files.get(0), ImageType.REVIEW));
 	}
 
 	@ImageValid
-	@PatchMapping("/{reviewId}")
-	public CommonResponse<UpdateReviewResponseDto> updateReview(@RequestBody UpdateReviewRequestDto requestDto,
+	@PatchMapping(value = "/{reviewId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public CommonResponse<UpdateReviewResponseDto> updateReview(
+		@RequestPart("requestDto") UpdateReviewRequestDto requestDto,
 		@PathVariable Long reviewId,
 		@RequestPart(value = "files", required = false) List<MultipartFile> files)
 		throws IOException {
+		if (files == null || files.isEmpty()) {
+			throw new CustomException(ImageErrorCode.IMAGE_NOT_FOUND);
+		}
 		return CommonResponse.ok(reviewService.updateReview(requestDto, reviewId, files.get(0), ImageType.REVIEW));
 	}
 
