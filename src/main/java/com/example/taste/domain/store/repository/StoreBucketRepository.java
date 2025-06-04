beab8d21b3db03dcdca2c0e6887013ae022f2ecb
kmchaejin <chaejin040@gmail.com>
2025-06-04 11:28:07 +0900

    🔥remove : jpaRepo에서 제공하는 메서드 중복 삭제

--- src/main/java/com/example/taste/domain/store/repository/StoreBucketRepository.java
+++ src/main/java/com/example/taste/domain/store/repository/StoreBucketRepository.java
@@ -1,7 +1,6 @@
 package com.example.taste.domain.store.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,9 +8,7 @@ import com.example.taste.domain.store.entity.StoreBucket;
 import com.example.taste.domain.user.entity.User;
 
 public interface StoreBucketRepository extends JpaRepository<StoreBucket, Long> {
-	Optional<StoreBucket> findById(Long id);
-
 	List<StoreBucket> findByUserAndNameStartingWith(User user, String name);
-	
+
 	List<StoreBucket> findAllByUserAndIsOpened(User targetUser, boolean b);
 }
