d5f18041256874f8918c35879444b74418153cca
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-06-05 10:49:24 +0900

    [Refactoring] Board에 Image 적용, 캐싱 테스트 (#50)
    * feat : image entity 구현
    
    * refactor : board 도메인 image 기능 추가
    
    * refactor : board 로직 image 기능 추가
    
    * refactor : 게시글 수정 로직 내 이미지 로직 삭제, 게시글 등록 캐싱 로직 추가
    
    * refactor : Board 등록 dto 정규식 수정
    
    * refactor : 리뷰 등록 시 pk 포인트 점수 부여
    
    * refactor : 게시글 Patch 어노테이션 consumes 수정
    
    * refactor : 불필요한 코드 주석 제거

--- src/main/java/com/example/taste/domain/board/controller/BoardController.java
+++ src/main/java/com/example/taste/domain/board/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.example.taste.domain.board.controller;
 
 import static com.example.taste.domain.board.dto.response.BoardSuccessCode.*;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,15 +17,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.taste.common.annotation.ImageValid;
 import com.example.taste.common.response.CommonResponse;
+import com.example.taste.config.security.CustomUserDetails;
 import com.example.taste.domain.board.dto.request.BoardRequestDto;
 import com.example.taste.domain.board.dto.request.BoardUpdateRequestDto;
 import com.example.taste.domain.board.dto.response.BoardListResponseDto;
 import com.example.taste.domain.board.dto.response.BoardResponseDto;
 import com.example.taste.domain.board.service.BoardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,19 +40,25 @@ public class BoardController {
 
 	private final BoardService boardService;
 
-	@PostMapping
-	public CommonResponse<Void> createBoard(@RequestBody BoardRequestDto requestDto) {
+	@ImageValid
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public CommonResponse<Void> createBoard(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@Valid @RequestPart("requestDto") BoardRequestDto requestDto,
+		@RequestPart(value = "files", required = false) List<MultipartFile> files) throws IOException {
 		Long storeId = 1L;
 		Long userId = 1L;
 		// TODO store, user 객체 받아오기
-		boardService.createBoard(userId, storeId, requestDto);
+		boardService.createBoard(userDetails.getId(), storeId, requestDto, files);
 		return CommonResponse.success(BOARD_CREATED);
 
 	}
 
 	// 게시글 단건 조회
 	@GetMapping("/{boardId}")
-	public CommonResponse<BoardResponseDto> findBoard(@PathVariable Long boardId) {
+	public CommonResponse<BoardResponseDto> findBoard(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable Long boardId) {
 		BoardResponseDto responseDto = boardService.findBoard(boardId);
 		return CommonResponse.ok(responseDto);
 	}
@@ -52,17 +66,19 @@ public class BoardController {
 	// 단순 게시글 목록
 	@GetMapping("/simple")
 	public CommonResponse<?> findBoardList(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
 		@PageableDefault(page = 0, size = 10) Pageable pageable
 	) {
 
 		// TODO user 코드 수정필요
 		Long userId = 1L;
-		List<BoardListResponseDto> responseDtoList = boardService.findBoardList(userId, pageable);
+		List<BoardListResponseDto> responseDtoList = boardService.findBoardList(userDetails.getId(), pageable);
 		return CommonResponse.ok(responseDtoList);
 	}
 
 	@GetMapping("/detailed")
 	public CommonResponse<?> findBoardDetailList(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
 		@RequestParam(required = false) String type,
 		@RequestParam(required = false) String status,
 		@RequestParam(defaultValue = "createdAt") String sort,
@@ -72,7 +88,8 @@ public class BoardController {
 		// TODO user 코드 수정필요
 		Long userId = 1L;
 		// TODO 기능 미구현
-		List<BoardResponseDto> responseDtoList = boardService.findBoardsFromFollowingUsers(userId, type, status, sort,
+		List<BoardResponseDto> responseDtoList = boardService.findBoardsFromFollowingUsers(userDetails.getId(), type,
+			status, sort,
 			order,
 			pageable);
 		// TODO 반환
@@ -80,20 +97,25 @@ public class BoardController {
 	}
 
 	@PatchMapping("/{boardId}")
-	public CommonResponse<Void> updateBoard(@RequestBody BoardUpdateRequestDto requestDto,
+	public CommonResponse<Void> updateBoard(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@Valid @RequestBody BoardUpdateRequestDto requestDto,
 		@PathVariable Long boardId
-	) {
+	) throws IOException {
 		// TODO user 코드 수정필요
 		Long userId = 1L;
-		boardService.updateBoard(userId, boardId, requestDto);
+
+		boardService.updateBoard(userDetails.getId(), boardId, requestDto);
 		return CommonResponse.success(BOARD_UPDATED);
 	}
 
 	@DeleteMapping("/{boardId}")
-	public CommonResponse<Void> deleteBoard(@PathVariable Long boardId) {
+	public CommonResponse<Void> deleteBoard(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable Long boardId) {
 		// TODO user 코드 수정필요
 		Long userId = 1L;
-		boardService.deleteBoard(userId, boardId);
+		boardService.deleteBoard(userDetails.getId(), boardId);
 		return CommonResponse.success(BOARD_DELETED);
 	}
 
