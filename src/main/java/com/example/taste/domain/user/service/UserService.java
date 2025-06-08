e4ad42a839b86b3af45f8a2bbd4c177b0dd3c264
YejinY00n <yoon17710@gmail.com>
2025-06-09 00:03:33 +0900

    Comment : 주석 수정

--- src/main/java/com/example/taste/domain/user/service/UserService.java
+++ src/main/java/com/example/taste/domain/user/service/UserService.java
@@ -75,7 +75,7 @@ public class UserService {
 				Image image = imageService.saveImage(file, ImageType.USER);
 				user.setImage(image);
 				userRepository.save(user);
-			} catch (IOException e) {    // 이미지 저장 실패하더라도 회원가입 진행 // TODO: 이미지 트랜잭션 확인 필요
+			} catch (IOException e) {    // 이미지 저장 실패하더라도 정보 업데이트 진행 // TODO: 이미지 트랜잭션 확인 필요
 				log.info("유저 이미지 저장에 실패하였습니다 (email: " + user.getEmail() + ")");
 			}
 		}
