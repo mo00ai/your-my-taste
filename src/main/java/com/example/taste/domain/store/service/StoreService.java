079dfd649823a0abc7f46f51a086f4c51328ab99
YejinYoon <68535398+YejinY00n@users.noreply.github.com>
2025-06-10 11:09:43 +0900

    [Refactoring] 엔티티 페처 적용 #53 (#61)
    * Refacotr: 일부 엔티티 페처 반영
    
    * Refacotr: 엔티티 페처 반영
    
    * Fix: 수정

--- src/main/java/com/example/taste/domain/store/service/StoreService.java
+++ src/main/java/com/example/taste/domain/store/service/StoreService.java
@@ -1,27 +1,25 @@
 package com.example.taste.domain.store.service;
 
-import static com.example.taste.domain.store.exception.StoreErrorCode.*;
-
 import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.taste.common.exception.CustomException;
+import com.example.taste.common.util.EntityFetcher;
 import com.example.taste.domain.store.dto.response.StoreResponse;
 import com.example.taste.domain.store.entity.Store;
 import com.example.taste.domain.store.repository.StoreRepository;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class StoreService {
-
+	private final EntityFetcher entityFetcher;
 	private final StoreRepository storeRepository;
 
 	public StoreResponse getStore(Long id) {
-		Store store = storeRepository.findById(id).orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+		Store store = entityFetcher.getStoreOrThrow(id);
 
 		List<String> imageUrls = store.getReviewList().stream()
 			.sorted((r1, r2) -> r2.getCreatedAt().compareTo(r1.getCreatedAt()))
@@ -35,13 +33,12 @@ public class StoreService {
 
 	@Transactional
 	public void deleteStore(Long id) {
-		Store store = storeRepository.findById(id).orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+		Store store = entityFetcher.getStoreOrThrow(id);
 		storeRepository.delete(store);
 	}
 
 	@Transactional(readOnly = true)
 	public Store findById(Long storeId) {
-		return storeRepository.findById(storeId)
-			.orElseThrow(() -> new CustomException(STORE_NOT_FOUND));
+		return entityFetcher.getStoreOrThrow(storeId);
 	}
 }
