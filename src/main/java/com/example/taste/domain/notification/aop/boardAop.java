aa9ef375f1d4bedc344ef57bcab49b0f774fa8a9
rlgkghkd <rlgkghkd@gmail.com>
2025-06-11 19:49:53 +0900

    feat: aop를 통한 구독자 알림 발행 기능 추가(게시글 생성)

---
+++ src/main/java/com/example/taste/domain/notification/aop/boardAop.java
@@ -0,0 +1,46 @@
+package com.example.taste.domain.notification.aop;
+
+import java.time.LocalDateTime;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.example.taste.common.service.RedisService;
+import com.example.taste.common.util.EntityFetcher;
+import com.example.taste.domain.notification.NotificationCategory;
+import com.example.taste.domain.notification.dto.NotificationEventDto;
+import com.example.taste.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Aspect
+@Component
+public class boardAop {
+	private final RedisService redisService;
+	private final EntityFetcher entityFetcher;
+
+	@AfterReturning(
+		pointcut = "execution(* com.example.taste.domain.board.service.BoardService.createBoard(..))",
+		returning = "result"
+	)
+
+	public void afterCreation(JoinPoint point, Object result) {
+		Object[] args = point.getArgs();
+		Long userId = (Long)args[0];
+		User user = entityFetcher.getUserOrThrow(userId);
+
+		NotificationEventDto dto = NotificationEventDto.builder()
+			.category(NotificationCategory.SUBSCRIBERS)
+			.content(user.getNickname() + "님의 새 글이 등록되었습니다.")
+			.createdAt(LocalDateTime.now())
+			.read(false)
+			.redirectUrl("/board") // 일단 그냥 board에 redirect. 이건 회의좀 해봐야 함.
+			.userId(userId)
+			.build();
+
+		redisService.publishNotification(dto);
+	}
+}
