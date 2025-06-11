9a7429f2602deaf94478f30b16d9d00ee36adfd6
YejinY00n <yoon17710@gmail.com>
2025-06-12 04:25:56 +0900

    Fix: UserMatchCondCategory 레포지토리 키 오류 수정

--- src/main/java/com/example/taste/domain/match/repository/UserMatchCondCategoryRepository.java
+++ src/main/java/com/example/taste/domain/match/repository/UserMatchCondCategoryRepository.java
@@ -2,5 +2,7 @@ package com.example.taste.domain.match.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserMatchCondCategoryRepository extends JpaRepository<UserMatchCondCategoryRepository, Long> {
+import com.example.taste.domain.match.entity.UserMatchCondCategory;
+
+public interface UserMatchCondCategoryRepository extends JpaRepository<UserMatchCondCategory, Long> {
 }
