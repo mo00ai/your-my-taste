dc08625ce6c44bb2334ac4e93bc9ba711d1f4478
rlgkghkd <rlgkghkd@gmail.com>
2025-06-12 14:48:00 +0900

    fix: 관리자 발행 알림 유저 역활 검증 설정

--- src/main/java/com/example/taste/domain/notification/controller/NotificationAdminController.java
+++ src/main/java/com/example/taste/domain/notification/controller/NotificationAdminController.java
@@ -1,5 +1,6 @@
 package com.example.taste.domain.notification.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class NotificationAdminController {
 	// 관리자용 알림 생성
 	// 마케팅, 시스템 알림 생성 가능
 	@PostMapping
+	@PreAuthorize("hasRole('ADMIN')")
 	public CommonResponse<Void> publishNotification(@RequestBody NotificationRequestDto dto) {
 		notificationAdminService.publishNotification(dto);
 		return CommonResponse.ok();
