0b1d4b90d59750fe0a5350a7260b18d88a90ac79
AhyunCho <jah112482@gmail.com>
2025-06-12 16:44:30 +0900

    refactor : 이미지 에러코드 수정

--- src/main/java/com/example/taste/domain/image/exception/ImageErrorCode.java
+++ src/main/java/com/example/taste/domain/image/exception/ImageErrorCode.java
@@ -18,9 +18,9 @@ public enum ImageErrorCode implements BaseCode {
 	FIVE_IMAGES_ALLOWED(HttpStatus.BAD_REQUEST, "I004", "맛집 추천 게시글의 이미지 개수는 5개까지 가능합니다."),
 	ONLY_ONE_IMAGE_ALLOWED(HttpStatus.BAD_REQUEST, "I005", "이미지 개수는 1개까지 가능합니다."),
 
-	FAILED_WRITE_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "I001", "파일을 저장하는 중 오류가 발생했습니다."),
-	IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "I002", "이미지를 찾을 수 없습니다."),
-	FAILED_DELETE_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "I003", "파일을 삭제하는 중 오류가 발생했습니다.");
+	FAILED_WRITE_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "I006", "파일을 저장하는 중 오류가 발생했습니다."),
+	IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "I007", "이미지를 찾을 수 없습니다."),
+	FAILED_DELETE_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "I008", "파일을 삭제하는 중 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
