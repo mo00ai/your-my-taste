c0d89a1302100072bf4745adb0954051f3a61cd1
rlgkghkd <rlgkghkd@gmail.com>
2025-06-12 14:39:49 +0900

    fix: 알림 예외 코드 메시지 수정

--- src/main/java/com/example/taste/domain/notification/exception/NotificationErrorCode.java
+++ src/main/java/com/example/taste/domain/notification/exception/NotificationErrorCode.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationErrorCode implements BaseCode {
-	NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "N001", "리뷰를 찾을 수 없습니다."),
+	NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "N001", "알림을 찾을 수 없습니다."),
 	;
 
 	private final HttpStatus httpStatus;
