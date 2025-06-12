dae145790fe8213aaaf8176ead1c0b7097a3f8f3
YejinY00n <yoon17710@gmail.com>
2025-06-13 03:16:15 +0900

    Fix: 로컬 병합 충돌 해결

--- src/main/java/com/example/taste/domain/store/repository/CategoryRepository.java
+++ src/main/java/com/example/taste/domain/store/repository/CategoryRepository.java
@@ -1,10 +1,7 @@
 package com.example.taste.domain.store.repository;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
 import java.util.Optional;
->>>>>>> dev
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
