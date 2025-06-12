4ad07bda51131545f7ae7e1672b7a5326d3f52f0
Chaemin07 <gkc05075@gmail.com>
2025-06-12 18:06:46 +0900

    style: 검색 조건 파라미터에 @ModelAttribute 및 설명 주석 추가
    - 키워드 기반 검색 조건 객체에 @ModelAttribute 어노테이션 명시

--- src/main/java/com/example/taste/domain/board/controller/BoardController.java
+++ src/main/java/com/example/taste/domain/board/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,8 @@ import com.example.taste.domain.board.dto.request.BoardRequestDto;
 import com.example.taste.domain.board.dto.request.BoardUpdateRequestDto;
 import com.example.taste.domain.board.dto.response.BoardListResponseDto;
 import com.example.taste.domain.board.dto.response.BoardResponseDto;
-import com.example.taste.domain.board.dto.search.BoardSearchCondition;
 import com.example.taste.domain.board.dto.response.OpenRunBoardResponseDto;
+import com.example.taste.domain.board.dto.search.BoardSearchCondition;
 import com.example.taste.domain.board.service.BoardService;
 import com.example.taste.domain.board.service.LikeService;
 
@@ -135,9 +136,10 @@ public class BoardController {
 		return CommonResponse.success(BOARD_UNLIKED);
 	}
 
+	// 키워드 조건 기반 검색
 	@GetMapping("/search")
 	public CommonResponse<PageResponse<BoardListResponseDto>> searchBoards(
-		@Valid BoardSearchCondition conditionDto,
+		@ModelAttribute @Valid BoardSearchCondition conditionDto,
 		@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable
 	) {
 		PageResponse<BoardListResponseDto> result = boardService.searchBoards(conditionDto,
