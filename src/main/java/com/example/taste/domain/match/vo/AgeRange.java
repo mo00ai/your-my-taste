ec9518af5402817cb23b756b755aa94f1b91e2fe
YejinY00n <yoon17710@gmail.com>
2025-06-13 13:02:46 +0900

    Feat: AgeRange 생성자 추가

--- src/main/java/com/example/taste/domain/match/vo/AgeRange.java
+++ src/main/java/com/example/taste/domain/match/vo/AgeRange.java
@@ -1,13 +1,26 @@
 package com.example.taste.domain.match.vo;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Positive;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class AgeRange {
+	@Positive
 	private Integer minAge;
+
+	@Positive
 	private Integer maxAge;
+
+	public AgeRange(Integer minAge, Integer maxAge) {
+		if (minAge == null || maxAge == null || (minAge > maxAge) || minAge % 10 != 0 || maxAge % 10 != 0) {
+			throw new IllegalArgumentException("minAge <= maxAge 이어야하며, minAge 와 maxAge 는 십자리 수 단위로만 가능합니다.");
+		}
+		this.minAge = minAge;
+		this.maxAge = maxAge;
+	}
 
 	public boolean includes(int age) {
 		return minAge <= age && age <= maxAge;
