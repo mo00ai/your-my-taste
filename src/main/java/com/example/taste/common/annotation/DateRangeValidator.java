a209ca415be7d42941c463be1d94a2521a844d6f
YejinY00n <yoon17710@gmail.com>
2025-06-13 14:27:08 +0900

    Feat: DateRange 에서 DateTime, LocalDateTime 둘다 커버하도록 변경

--- src/main/java/com/example/taste/common/annotation/DateRangeValidator.java
+++ src/main/java/com/example/taste/common/annotation/DateRangeValidator.java
@@ -1,13 +1,15 @@
 package com.example.taste.common.annotation;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.ChronoUnit;
 
 import jakarta.validation.ConstraintDeclarationException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class DateRangeValidator implements ConstraintValidator<DateRange, LocalDateTime> {
+public class DateRangeValidator implements ConstraintValidator<DateRange, Object> {
 	private DateRange annotation;
 	private int min;
 	private int max;
@@ -27,11 +29,31 @@ public class DateRangeValidator implements ConstraintValidator<DateRange, LocalD
 	}
 
 	@Override
-	public boolean isValid(LocalDateTime value, ConstraintValidatorContext context) {
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		if (value == null) {
 			return true;
 		}
 
+		if (value instanceof LocalDate) {
+			return validLocalDate((LocalDate)value);
+		} else if (value instanceof LocalDateTime) {
+			return validLocalDateTime((LocalDateTime)value);
+		} else {
+			return false;        // 지원하지 않는 타입
+		}
+	}
+
+	private boolean validLocalDate(LocalDate value) {
+		// 시간이 min unit ~ max unit 내의 날짜인지 검사
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime minBound = now.plus(min, unit);
+		LocalDateTime maxBound = now.plus(max, unit);
+
+		return (value.isAfter(ChronoLocalDate.from(minBound)) || value.isEqual(ChronoLocalDate.from(minBound)))
+			&& (value.isBefore(ChronoLocalDate.from(maxBound)) || value.isEqual(ChronoLocalDate.from(maxBound)));
+	}
+
+	public boolean validLocalDateTime(LocalDateTime value) {
 		// 시간이 min unit ~ max unit 내의 날짜인지 검사
 		LocalDateTime now = LocalDateTime.now();
 		LocalDateTime minBound = now.plus(min, unit);
@@ -39,5 +61,6 @@ public class DateRangeValidator implements ConstraintValidator<DateRange, LocalD
 
 		return (value.isAfter(minBound) || value.isEqual(minBound))
 			&& (value.isBefore(maxBound) || value.isEqual(maxBound));
+
 	}
 }
