9afe8b8392f755e324dddb8fdcf5f5ef0cfff979
YejinY00n <yoon17710@gmail.com>
2025-06-13 17:17:27 +0900

    Refactor: 리스너컨테이너에 등록 하는 부분을 redisConfig 로 이동

--- src/main/java/com/example/taste/domain/notification/redis/RedisNotificationListener.java
+++ src/main/java/com/example/taste/domain/notification/redis/RedisNotificationListener.java
@@ -1,14 +1,9 @@
 package com.example.taste.domain.notification.redis;
 
-import jakarta.annotation.PostConstruct;
-
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
-
-import com.example.taste.common.constant.RedisChannel;
 
 @Component
 @RequiredArgsConstructor
@@ -16,13 +11,13 @@ public class RedisNotificationListener {
 	private final RedisMessageListenerContainer redisMessageListenerContainer;
 	private final NotificationSubscriber notificationSubscriber;
 
-	@PostConstruct
-	public void registerSubscriber() {
-		// redis 에서 message 가 발생하는 것을 듣고 있을 클래스들을 지정
-		// message 가 notification 채널에서 발생하는 경우에만 onMessage 메서드 호출
-		redisMessageListenerContainer.addMessageListener(
-			notificationSubscriber,
-			new ChannelTopic(RedisChannel.NOTIFICATION_CHANNEL)
-		);
-	}
+	// @PostConstruct
+	// public void registerSubscriber() {
+	// 	// redis 에서 message 가 발생하는 것을 듣고 있을 클래스들을 지정
+	// 	// message 가 notification 채널에서 발생하는 경우에만 onMessage 메서드 호출
+	// 	redisMessageListenerContainer.addMessageListener(
+	// 		notificationSubscriber,
+	// 		new ChannelTopic(RedisChannel.NOTIFICATION_CHANNEL)
+	// 	);
+	// }
 }
