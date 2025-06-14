78ab081dc6632500b6adac51baeccb913b7952ef
YejinY00n <yoon17710@gmail.com>
2025-06-14 22:28:18 +0900

    Fix: 빌드 오류 수정

--- src/main/java/com/example/taste/config/WebSocketConfig.java
+++ src/main/java/com/example/taste/config/WebSocketConfig.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,7 +15,6 @@ import com.example.taste.common.websocket.WebSocketAuthInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@EnableWebSocketSecurity
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	private final WebSocketAuthInterceptor authInterceptor;
