dae599879270b97c0d61bf41f253231bb782756c
kmchaejin <chaejin040@gmail.com>
2025-06-18 10:49:44 +0900

    ️ chore : properties 변경

--- src/test/java/com/example/taste/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/example/taste/domain/board/service/BoardServiceTest.java
@@ -46,8 +46,8 @@ import com.example.taste.fixtures.UserFixture;
 
 import jakarta.transaction.Transactional;
 
-@ActiveProfiles("test-int")
-//@ActiveProfiles("test-int-docker")
+//@ActiveProfiles("test-int")
+@ActiveProfiles("test-int-docker")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BoardServiceTest {
 	@LocalServerPort
