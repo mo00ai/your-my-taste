49d2dde4959789f92b1df18affd67f42f3469b01
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-06-19 18:59:22 +0900

    [Feature] 배치, 쿼츠 구조 설정 (#103)
    * test : pk 서비스 테스트 코드 작성
    
    * fix : 코드래빗 기반 리팩토링
    
    * fix : 이미지 jpql -> querydsl 리팩토링, 및 map이 아닌 클래스로 데이터 저장 적용
    
    * fix : eventAop 임시 주석 처리 -> 리팩토링 예정
    
    * fix : 테스트 환경 설정, 테스트 코드 수정
    
    * chore : 테스트 환경 설정
    
    * chore : 테스트 환경 설정, ci.yml 테스트 반영
    
    * chore : ci.yml 에 docker-compose 설치
    
    * chore : ci.yml 환경변수 설정 github secret 쓰도록 설정
    
    * fix : 모든 통합 테스트에 @ActiveProfiles("test-int") 추가
    
    * fix : BoardFixture.java 수정
    
    * chore: ci.yml test-int만 properties 인식하도록 수정
    
    * chore: 경로 명시
    
    * chore: 백슬래시 삭제
    
    * chore: .env.test 환경변수 export 할 수 있도록 수정
    
    * chore: .env.test를 run: 안에서 직접 export로 하나씩 읽도록 수정
    
    * chore: 환경변수 강제 주입하도록 수정
    
    * chore: 환경변수 강제 주입하도록 수정
    
    * chore: run test 수정..
    
    * chore: localhost -> 아이피로 인식되도록 수정
    
    * chore: docker compose up 디버깅
    
    * chore: docker compose up 디버깅
    
    * chore: 재수정..
    
    * chore: 재수정..
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 3306으로 포트 번호 변경
    
    * chore: 순정
    
    * chore: 순정
    
    * chore: 순정
    
    * chore: 제발 되세요...
    
    * chore: 제발 되세요...2
    
    * chore: host.docker.mysql 임시시도
    
    * chore: app 서비스 이용하도록
    
    * chore: app 서비스 이용하도록
    
    * chore: app 서비스 이용하도록
    
    * chore: app 서비스 이용하도록
    
    * chore: 헬스채크 수정
    
    * chore: env username password 삭제
    
    * chore: 포트 외부 수정
    
    * chore: 포트 외부 수정
    
    * chore: sout
    
    * chore: sout
    
    * chore: flyway삭제
    
    * chore: 다시 localhost변경
    
    * chore: 더미 환경변수
    
    * chore: 더미 환경변수
    
    * chore: ip 다 컨테이너 이름으로
    
    * chore: 갈아 엎음
    
    * chore: 갈아 엎음
    
    * chore: 갈아 엎음
    
    * chore: 갈아 엎음
    
    * chore: 갈아 엎음
    
    * chore: 갈아 엎음
    
    * chore: 환경변수 수정
    
    * chore: 더미 환경변수
    
    * chore: 채진님 jooq 설정 ci 반영
    
    * chore: 최종 수정
    
    * fix : 이미지 주소 관련 수정, 및 ci conditional하게 jooq 제외하도록
    
    * fix : yml jooq 환경변수 주입 이것도 다시 확인할 필요
    
    * chore : ci 테스트, 로컬 테스트 환경 분리
    
    * chore : build.gradle jooq 경로 수정
    
    * fix : Jooq 파일 삭제, 지금이 최적의 상태 이제부터 수정 들어갑니다. 돌아가고 싶으면 여기를
    
    * fix : ci jooq 반영 수정
    
    * fix : compose.test.ci 연결
    
    * fix : ci db host 이름이 docker-compose.test.ci 의 mysql service 명과 똑같도록 설정
    
    * fix : ci db host localhost로 변경
    
    * fix : init.sql db ddl 문서 추가 스키마 생성 용
    
    * fix : 에러나는 테스트 BoardServiceTest 메서드 임시 주석 처리
    
    * chore : sql 수정
    
    * feat : batch 공통 설정
    
    * feat : 배치 공통 설정 구현
    
    * feat : 배치라운처잡 이름 수정
    
    * chore : test.properties에도 환경변수 추가
    
    * chore : test.properties에도 환경변수 수정
    
    * chore : ci에서 테이블 대소문자 구분하도록

---
+++ src/main/java/com/example/taste/domain/event/batch/EventWinnerBatchConfig.java
@@ -0,0 +1,69 @@
+package com.example.taste.domain.event.batch;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class EventWinnerBatchConfig extends DefaultBatchConfiguration {
+
+	//재시도 로직
+	@Bean
+	public Job EventWinnerJob(JobRepository repo, Step step) {
+		return new JobBuilder("winnerSelectionJob", repo)
+			.start(step)
+			.build();
+	}
+
+	@Bean
+	public Step step(JobRepository repo, PlatformTransactionManager tx) {
+		return new StepBuilder("winnerStep", repo)
+			.<String, String>chunk(10, tx)
+			.reader(reader())
+			.processor(processor())
+			.writer(writer())
+			.build();
+	}
+
+	@Bean
+	public ItemReader<String> reader() {
+
+		return new ListItemReader<>(List.of("EVENT1", "EVENT2"));
+	}
+
+	@Bean
+	public ItemProcessor<String, String> processor() {
+
+		return item -> item.toLowerCase();
+	}
+
+	@Bean
+	public ItemWriter<String> writer() {
+
+		return items -> items.forEach(System.out::println);
+	}
+
+	//스텝의 하위 단계
+	//스텝을 만들면 스텝 하나에 테스크렛 하나가 만드러짐
+	// @Bean
+	// public Tasklet myTasklet() {
+	// 	return (contribution, context) -> {
+	// 		// 여기 안에 로직을 다 씀
+	// 		System.out.println("DB 조회 → 조건 체크 → 포인트 지급");
+	// 		return RepeatStatus.FINISHED;
+	// 	};
+	// }
+
+}
