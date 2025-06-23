da5602c6bdf31bc3ec59118e0b4842704bb63962
아현느 <134674305+mo00ai@users.noreply.github.com>
2025-06-23 23:50:34 +0900

    [Refactoring] 이미지 조회 cloudfront 적용, pk 서비스 enum으로 관리 리팩토링 (#116)
    * refactor : presgined-url 적용
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : ci 테스트
    
    * refactor : 이미지 저장 url 조정, cloudfront 도메인 환경변수 추가
    
    * refactor : pkcriteria 관련 주석처리
    
    * refactor : 테스트 코드 로컬에서 잘 돌아가도록 수정
    
    * refactor : build.gradle jooq관련 db 수정
    
    * refactor : pkCriteria entity 삭제, batch 테이블 수동 설치로 ddl 변경 init.sql
    
    * refactor : application-test-int-docker.properties , mysql, redis 수정
    
    * refactor : application-test-int-docker.properties , mysql, redis 수정
    
    * refactor : application-test-int-docker.properties , mysql, redis 수정
    
    * refactor : application-test-int-docker.properties , mysql, redis 수정
    
    * refactor : activeprofiles다시없애기
    
    * refactor : postgre로 db변경
    
    * refactor : postgre로 db변경
    
    * refactor : docker-compose.test.ci.yml 수정
    
    * refactor : docker-compose.test.ci.yml 수정
    
    * refactor : docker-compose.test.ci.yml 수정
    
    * refactor : docker-compose.test.ci.yml 수정
    
    * refactor : docker-compose.test.ci.yml 수정
    
    * refactor : docker-compose.test.ci.yml 수정

--- src/main/java/com/example/taste/common/constant/ImageConst.java
+++ src/main/java/com/example/taste/common/constant/ImageConst.java
@@ -2,6 +2,5 @@ package com.example.taste.common.constant;
 
 public class ImageConst {
 	public static final int MAX_IMAGE_CNT = 5;
-	// public static final int MAX_IMAGE_SIZE = 2 * 1024 * 1024;
-	public static final int MAX_IMAGE_SIZE = 100 * 1024 * 1024;
+	public static final int MAX_IMAGE_SIZE = 2 * 1024 * 1024;
 }
--- src/test/java/com/example/taste/domain/pk/service/PkServiceUnitTest.java
+++ src/test/java/com/example/taste/domain/pk/service/PkServiceUnitTest.java
@@ -1,80 +1,80 @@
-package com.example.taste.domain.pk.service;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
-import static org.mockito.BDDMockito.*;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import com.example.taste.domain.pk.dto.response.PkCriteriaResponseDto;
-import com.example.taste.domain.pk.entity.PkTerm;
-import com.example.taste.domain.pk.enums.PkType;
-import com.example.taste.domain.pk.repository.PkTermRepository;
-
-@ActiveProfiles("test-int")
-@ExtendWith(MockitoExtension.class)
-class PkServiceUnitTest {
-
-	@InjectMocks
-	private PkService pkService;
-
-	@Mock
-	private PkCacheService pkCacheService;
-
-	@Mock
-	private PkTermRepository pkTermRepository;
-
-	@Test
-	void findAllPkCriteria_success() {
-		// given
-		List<PkCriteriaResponseDto> mockList = List.of(
-			new PkCriteriaResponseDto(1L, "POST", 30, true),
-			new PkCriteriaResponseDto(1L, "REVIEW", 20, true)
-		);
-		given(pkCacheService.findAllPkCriteria()).willReturn(mockList);
-
-		// when
-		List<PkCriteriaResponseDto> result = pkService.findAllPkCriteria();
-
-		// then
-		assertThat(result).hasSize(2);
-		assertThat(result.get(0).getType()).isEqualTo("POST");
-	}
-
-	@Test
-	void getPointByPkType_success() {
-		// given
-		List<PkCriteriaResponseDto> mockList = List.of(
-			new PkCriteriaResponseDto(1L, "POST", 30, true),
-			new PkCriteriaResponseDto(1L, "REVIEW", 20, true)
-		);
-		given(pkCacheService.findAllPkCriteria()).willReturn(mockList);
-
-		// when
-		int point = pkService.getPointByPkType(PkType.POST);
-
-		// then
-		assertThat(point).isEqualTo(30);
-	}
-
-	@Test
-	void getMaxPkTerm_success() {
-		// given
-		PkTerm latestTerm = PkTerm.builder().term(5).build();
-		given(pkTermRepository.findTopByOrderByTermDesc()).willReturn(Optional.of(latestTerm));
-
-		// when
-		int nextTerm = ReflectionTestUtils.invokeMethod(pkService, "getMaxPkTerm");
-
-		// then
-		assertThat(nextTerm).isEqualTo(6);
-	}
-}
+// package com.example.taste.domain.pk.service;
+//
+// import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+// import static org.mockito.BDDMockito.*;
+//
+// import java.util.List;
+// import java.util.Optional;
+//
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.junit.jupiter.MockitoExtension;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.util.ReflectionTestUtils;
+//
+// import com.example.taste.domain.pk.dto.response.PkCriteriaResponseDto;
+// import com.example.taste.domain.pk.entity.PkTerm;
+// import com.example.taste.domain.pk.enums.PkType;
+// import com.example.taste.domain.pk.repository.PkTermRepository;
+//
+// @ActiveProfiles("test-int")
+// @ExtendWith(MockitoExtension.class)
+// class PkServiceUnitTest {
+//
+// 	@InjectMocks
+// 	private PkService pkService;
+//
+// 	@Mock
+// 	private PkCacheService pkCacheService;
+//
+// 	@Mock
+// 	private PkTermRepository pkTermRepository;
+//
+// 	@Test
+// 	void findAllPkCriteria_success() {
+// 		// given
+// 		List<PkCriteriaResponseDto> mockList = List.of(
+// 			new PkCriteriaResponseDto(1L, "POST", 30, true),
+// 			new PkCriteriaResponseDto(1L, "REVIEW", 20, true)
+// 		);
+// 		given(pkCacheService.findAllPkCriteria()).willReturn(mockList);
+//
+// 		// when
+// 		List<PkCriteriaResponseDto> result = pkService.findAllPkCriteria();
+//
+// 		// then
+// 		assertThat(result).hasSize(2);
+// 		assertThat(result.get(0).getType()).isEqualTo("POST");
+// 	}
+//
+// 	@Test
+// 	void getPointByPkType_success() {
+// 		// given
+// 		List<PkCriteriaResponseDto> mockList = List.of(
+// 			new PkCriteriaResponseDto(1L, "POST", 30, true),
+// 			new PkCriteriaResponseDto(1L, "REVIEW", 20, true)
+// 		);
+// 		given(pkCacheService.findAllPkCriteria()).willReturn(mockList);
+//
+// 		// when
+// 		int point = pkService.getPointByPkType(PkType.POST);
+//
+// 		// then
+// 		assertThat(point).isEqualTo(30);
+// 	}
+//
+// 	@Test
+// 	void getMaxPkTerm_success() {
+// 		// given
+// 		PkTerm latestTerm = PkTerm.builder().term(5).build();
+// 		given(pkTermRepository.findTopByOrderByTermDesc()).willReturn(Optional.of(latestTerm));
+//
+// 		// when
+// 		int nextTerm = ReflectionTestUtils.invokeMethod(pkService, "getMaxPkTerm");
+//
+// 		// then
+// 		assertThat(nextTerm).isEqualTo(6);
+// 	}
+// }
