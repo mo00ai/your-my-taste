b2e7f496ac1d3ba1c226b4918c295154c1c07685
YejinYoon <68535398+YejinY00n@users.noreply.github.com>
2025-06-23 11:44:12 +0900

    [Refactoring] 채팅 리팩토링 #106 (#110)
    * Refactor: 엔티티 페처 제거 및 불필요한 필드 제거
    
    * Refactor: CORS Config 추가 및 프로필 별 적용
    
    * Refactor: 중복/불필요 웹소켓 처리 파일 삭제 및 디렉토리 이동
    
    * Feat: STOMP 커맨드 핸들러 및 Disconnect 이벤트 핸들러 추가
    
    * Comment: 주석 삭제
    
    * Refactor: CorsConfig local에만 적용되도록 profile 수정
    
    * Refactor: 채팅 구독/발행 static화 및 미리 컴파일
    
    * Refactor: 구독 커맨드에서 예외 발생 시 null 리턴하여 구독 방지

--- src/main/java/com/example/taste/domain/chat/service/ChatService.java
+++ src/main/java/com/example/taste/domain/chat/service/ChatService.java
@@ -1,5 +1,6 @@
 package com.example.taste.domain.chat.service;
 
+import static com.example.taste.domain.party.exception.PartyErrorCode.PARTY_NOT_FOUND;
 import static com.example.taste.domain.party.exception.PartyErrorCode.UNAUTHORIZED_PARTY_INVITATION;
 import static com.example.taste.domain.user.exception.UserErrorCode.NOT_FOUND_USER;
 
@@ -10,28 +11,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.example.taste.common.exception.CustomException;
-import com.example.taste.common.util.EntityFetcher;
 import com.example.taste.domain.chat.dto.ChatCreateRequestDto;
 import com.example.taste.domain.chat.dto.ChatResponseDto;
 import com.example.taste.domain.chat.entity.Chat;
 import com.example.taste.domain.chat.repository.ChatRepository;
 import com.example.taste.domain.party.entity.Party;
-import com.example.taste.domain.party.entity.PartyInvitation;
 import com.example.taste.domain.party.repository.PartyInvitationRepository;
+import com.example.taste.domain.party.repository.PartyRepository;
 import com.example.taste.domain.user.entity.User;
 import com.example.taste.domain.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
-	private final EntityFetcher entityFetcher;
 	private final ChatRepository chatRepository;
-	private final PartyInvitationRepository partyInvitationRepository;
 	private final UserRepository userRepository;
+	private final PartyRepository partyRepository;
+	private final PartyInvitationRepository partyInvitationRepository;
 
 	// 보낸 메세지 저장
 	public ChatResponseDto saveMessage(User user, ChatCreateRequestDto dto) {
-		Party party = entityFetcher.getPartyOrThrow(dto.getPartyId());
+		Party party = partyRepository.findById(dto.getPartyId())
+			.orElseThrow(() -> new CustomException(PARTY_NOT_FOUND));
 		if (!partyInvitationRepository.isConfirmedPartyMember(dto.getPartyId(), user.getId())) {
 			throw new CustomException(UNAUTHORIZED_PARTY_INVITATION);
 		}
@@ -43,10 +44,8 @@ public class ChatService {
 	}
 
 	public List<ChatResponseDto> getChats(User user, Long partyId) {
-		Party party = entityFetcher.getPartyOrThrow(partyId);
-		List<PartyInvitation> partyInvitationList =
-			partyInvitationRepository.findByPartyId(partyId);
-
+		Party party = partyRepository.findById(partyId)
+			.orElseThrow(() -> new CustomException(PARTY_NOT_FOUND));
 		if (!partyInvitationRepository.isConfirmedPartyMember(partyId, user.getId())) {
 			throw new CustomException(UNAUTHORIZED_PARTY_INVITATION);
 		}
