12edb275253842e496d60b1693f421745c54fcfa
kmchaejin <chaejin040@gmail.com>
2025-06-29 07:13:40 +0900

    🐛 fix : npe 오류 해결

--- src/main/java/com/example/taste/domain/board/dto/response/OpenRunBoardResponseDto.java
+++ src/main/java/com/example/taste/domain/board/dto/response/OpenRunBoardResponseDto.java
@@ -8,6 +8,7 @@ import com.example.taste.domain.board.entity.AccessPolicy;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @JsonInclude(value = NON_NULL)
@@ -19,14 +20,14 @@ public class OpenRunBoardResponseDto {
 	private final String title;
 	private final LocalDateTime openTime;
 	private final AccessPolicy accessPolicy;
-	private final Integer openLimit; // 접근 허용 인원 수 or 시간
+	@Setter
+	private Integer openLimit; // 접근 허용 인원 수 or 시간
+	@Setter
 	private Long remainingSlot; // 잔여 인원 수
 
 	// 공개 예정 게시글이면 openLimit, remainingSlot에 null 삽입
 	public OpenRunBoardResponseDto(Long userId, String userName, String userImageUrl, Long boardId, String title,
 		LocalDateTime openTime, AccessPolicy accessPolicy, Integer openLimit) {
-		boolean isOpened = !openTime.isAfter(LocalDateTime.now());
-
 		this.userId = userId;
 		this.userName = userName;
 		this.userImageUrl = userImageUrl;
@@ -34,11 +35,6 @@ public class OpenRunBoardResponseDto {
 		this.title = title;
 		this.openTime = openTime;
 		this.accessPolicy = accessPolicy;
-		this.openLimit = isOpened ? openLimit : null;
-	}
-
-	public void setRemainingSlot(Long remainingSlot) {
-		boolean isOpened = !openTime.isAfter(LocalDateTime.now());
-		this.remainingSlot = isOpened ? remainingSlot : null;
+		this.openLimit = openLimit;
 	}
 }
