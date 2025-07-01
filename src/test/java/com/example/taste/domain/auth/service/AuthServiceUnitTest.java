5b778664a2b7df919389259aba5d8fa8ad135dad
YejinY00n <yoon17710@gmail.com>
2025-07-02 07:17:52 +0900

    Test: AuthnService 테스트 코드 추가

--- src/test/java/com/example/taste/domain/auth/service/AuthServiceUnitTest.java
+++ src/test/java/com/example/taste/domain/auth/service/AuthServiceUnitTest.java
@@ -3,38 +3,46 @@ package com.example.taste.domain.auth.service;
 import static com.example.taste.domain.user.exception.UserErrorCode.DEACTIVATED_USER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.example.taste.common.exception.CustomException;
 import com.example.taste.domain.auth.dto.SigninRequestDto;
-import com.example.taste.domain.user.entity.User;
+import com.example.taste.domain.user.dto.UserSigninProjectionDto;
 import com.example.taste.domain.user.repository.UserRepository;
-import com.example.taste.fixtures.UserFixture;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceUnitTest {
 	@InjectMocks
 	private AuthService authService;
-	@MockitoBean
+	@Mock
 	private UserRepository userRepository;
-	@MockitoBean
-	private PasswordEncoder passwordEncoder;
 
 	@Test
 	@DisplayName("탈퇴한 사용자는 로그인 실패")
 	public void failToSignInWithSoftDeletedUser() {
 		// given
-		User deletedUser = UserFixture.createSoftDeletedUser(null);
+		UserSigninProjectionDto deletedUser = mock(UserSigninProjectionDto.class);
+		when(deletedUser.getEmail()).thenReturn("test@email.com");
+		when(deletedUser.getPassword()).thenReturn("password");
+		when(deletedUser.getDeletedAt()).thenReturn(LocalDateTime.now());
+
 		SigninRequestDto requestDto = new SigninRequestDto(deletedUser.getEmail(), deletedUser.getPassword());
 		MockHttpServletRequest request = new MockHttpServletRequest();
+
+		when(userRepository.findSigninProjectionByEmail(deletedUser.getEmail()))
+			.thenReturn(Optional.of(deletedUser));
 
 		// when & then
 		CustomException e = assertThrows(CustomException.class,
