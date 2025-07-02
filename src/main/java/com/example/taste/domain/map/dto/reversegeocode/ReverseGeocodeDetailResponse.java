19e0ebdeb524cc41332c976c1008699bc6196951
Chaemin07 <gkc05075@gmail.com>
2025-07-02 14:07:50 +0900

    test: 네이버 지도 api 단위 테스트 코드 작성

--- src/main/java/com/example/taste/domain/map/dto/reversegeocode/ReverseGeocodeDetailResponse.java
+++ src/main/java/com/example/taste/domain/map/dto/reversegeocode/ReverseGeocodeDetailResponse.java
@@ -5,14 +5,16 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReverseGeocodeDetailResponse {
 	private ReverseGeocodeStatus status;
-	private List<ReverseGeocodeResult> results;
+	public List<ReverseGeocodeResult> results;
 }
