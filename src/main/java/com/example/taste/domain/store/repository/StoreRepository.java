285a56dfee8898146148acc5fe19ffae006707d7
kmchaejin <chaejin040@gmail.com>
2025-07-07 13:20:01 +0900

    chore : 사용 안하는 메서드 삭제

--- src/main/java/com/example/taste/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/taste/domain/store/repository/StoreRepository.java
@@ -1,12 +1,8 @@
 package com.example.taste.domain.store.repository;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.taste.domain.store.entity.Store;
 
@@ -14,14 +10,4 @@ public interface StoreRepository extends JpaRepository<Store, Long>, StoreReposi
 	// 중복 체크용 메서드
 	boolean existsByNameAndMapxAndMapy(String storeName, BigDecimal longitude, BigDecimal latitude);
 
-	List<Store> findByEmbeddingVectorIsNull();
-
-	Page<Store> findByEmbeddingVectorIsNull(Pageable pageable);
-
-	@Query("""
-		    SELECT s FROM Store s
-		    JOIN FETCH s.category c
-		    WHERE s.embeddingVector IS NULL
-		""")
-	Page<Store> findByEmbeddingVectorIsNullWithCategory(Pageable pageable);
 }
